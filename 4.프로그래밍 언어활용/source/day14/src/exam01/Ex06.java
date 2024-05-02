ce6cbcdb0d67ce80ca0300e4d40fb2ae47c63a6b
siwon9 <siwon10823@gmail.com>
2024-05-02 19:18:05 +0900

    수업 소스

--- 4.프로그래밍 언어활용/source/day14/src/exam01/Ex06.java
+++ 4.프로그래밍 언어활용/source/day14/src/exam01/Ex06.java
@@ -4,7 +4,7 @@ import java.util.function.IntPredicate;
 
 public class Ex06 {
     public static void main(String[] args) {
-        IntPredicate cond1 = x -> x >= 10;
+        IntPredicate cond1 = x -> x >= 10; //
         IntPredicate cond2 = x -> x <= 100;
         IntPredicate cond3 = cond1.and(cond2); // x >= 10 && <= 100
         IntPredicate cond4 = cond2.negate(); // x > 100
