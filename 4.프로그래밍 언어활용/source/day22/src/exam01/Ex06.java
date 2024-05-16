9f048eb60a39edc8599a0c388669bc530186e15c
siwon9 <siwon10823@gmail.com>
2024-05-16 17:00:59 +0900

    5월 16일 자바 수업내용

--- 4.프로그래밍 언어활용/source/day22/src/exam01/Ex06.java
+++ 4.프로그래밍 언어활용/source/day22/src/exam01/Ex06.java
@@ -4,7 +4,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class Ex06 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         File tmpFile = File.createTempFile("tmp",".log", new File("home/fstudy"));
+
+        // tmpFile.delete(); // 바로 삭제
+        tmpFile.deleteOnExit(); // 어플 종료되면 삭제
+
+        Thread.sleep(5000);
+
+        System.out.println("종료!");
     }
 }
