64991e5a3710500e57b46eaecc2059e429b46339
siwon9 <siwon10823@gmail.com>
2024-05-19 20:36:12 +0900

    문제풀이 및 복습내용

--- 4.프로그래밍 언어활용/source/Coding_2/src/Question5/Q1.java
+++ 4.프로그래밍 언어활용/source/Coding_2/src/Question5/Q1.java
@@ -1,4 +1,21 @@
 package Question5;
 
+import java.util.Arrays;
+
+//Q1. 머쓱이는 행운의 숫자 7을 가장 좋아합니다. 정수 배열 array가 매개변수로 주어질 때,
+// 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
 public class Q1 {
-}
+    public static void main(String[] args) {
+
+                int[] array = {7, 77, 17};
+                int num = 7;
+                String numStr = String.valueOf(num);
+
+                long count = Arrays.stream(array)
+                        .mapToObj(String::valueOf)
+                        .mapToLong(s -> s.chars().filter(ch -> ch == numStr.charAt(0)).count())
+                        .sum();
+
+                System.out.println(count);
+            }
+        }
--- 4.프로그래밍 언어활용/source/Coding_2/src/Question5/Q2.java
+++ 4.프로그래밍 언어활용/source/Coding_2/src/Question5/Q2.java
@@ -1,4 +1,9 @@
 package Question5;
-
+//Q2. 문자열 my_string이 매개변수로 주어질 때,
+// 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
 public class Q2 {
+    public static void main(String[] args) {
+        String my_string = "AAAbbb";
+
+    }
 }
