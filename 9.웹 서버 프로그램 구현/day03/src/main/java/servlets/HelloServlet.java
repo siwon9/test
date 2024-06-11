2f88a318b8ee057203d507cc0104719752fe2e06
siwon9 <siwon10823@gmail.com>
2024-06-11 16:28:57 +0900

    수업내용

--- 9.웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
+++ 9.웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,9 +12,37 @@ public class HelloServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        System.out.println("안녕하세요~!");
+        // System.out.println("안녕하세요~!");
+        System.out.println("doGet()");
+
+        // 모든 서블릿이 공유 가능한 초기 설정 값 - context-param
+        String common1 = req.getServletContext().getInitParameter("common1");
+        System.out.println(common1);
+        String common2 = req.getServletContext().getInitParameter("common1");
+        System.out.println(common2);
+    }
+/*
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init()");
+    }
+ */
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        // HelloServlet만 조회 가능
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s%n", key1, key2);
+
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy()");
     }
 }
+
 
 
 
---
+++ 9.웹 서버 프로그램 구현/day03/src/main/java/servlets/JoinServlet.java
@@ -0,0 +1,38 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class JoinServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=UTF-8"); // 이렇게 한번에 정의해도 된다.
+        // resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>회원가입</h1>");
+        out.println("<form method='POST' action='join'>");
+        out.println("이메일:<input type='text' name='email'><br>");
+        out.println("비밀번호:<input type='password' name='password'><br>");
+        out.println("비밀번호 확인:<input type='password' name='confirmPassword'><br>");
+        out.println("회원명:<input type='text' name='userName'><br>");
+        out.println("<button type='submit'>가입하기</button>");
+        out.println("</form>");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String confirmPassword = req.getParameter("confirmPassword");
+        String userName = req.getParameter("userName");
+    }
+}
