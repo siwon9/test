47e491b1a7774e09acbae606c57dc57dde9e703e
siwon9 <siwon10823@gmail.com>
2024-07-09 11:15:28 +0900

    수업

--- 11.프레임워크 프로그래밍/day03_1/src/main/java/config/ProxyCalculator.java
+++ 11.프레임워크 프로그래밍/day03_1/src/main/java/config/ProxyCalculator.java
@@ -1,9 +1,8 @@
 package config;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class ProxyCalculator {
@@ -11,12 +10,49 @@ public class ProxyCalculator {
     @Pointcut("execution(* exam01..*(..))") // exam01쪽 모든 메서드를 호출해준다.
     public void publicTarget() {}
 
-    @Around("publicTarget()") //
+    @Before("publicTarget()")
+    public void before(JoinPoint joinPoint){
+        System.out.println("Before..");
+    }
+
+    @After("publicTarget()")
+    public void after(JoinPoint joinPoint) {
+        System.out.println("After..");
+    }
+
+    @AfterReturning("publicTarget()")
+    public void afterReturning(JoinPoint joinPoint, Object returnValue) throws Throwable {
+        System.out.println("AfterReturning: " + returnValue);
+    }
+
+//    @AfterThrowing
+//    public void afterThrowing
+
+    @Around("publicTarget()")
     public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("유입!");
 
-        Object result = joinPoint.proceed(); // 핵심 기능 디신 실행 - factorial
+//        Signature sig = joinPoint.getSignature(); // 호출된 메서드 정의 정보
+//        System.out.println(sig.toLongString());
+//
+//        Object[] args = joinPoint.getArgs(); // 인수 정보
+//        System.out.println(Arrays.toString(args));
+//
+//        Object obj = joinPoint.getTarget(); // 호출한 메서드를 가지고 있는 객체
+//        System.out.println(obj.getClass());
 
-        return result;
+        long stime = System.nanoTime(); // 공통 기능
+        try {
+            Object result = joinPoint.proceed(); // 핵심 기능 디신 실행 - factorial
+
+            boolean re = false;
+            if(!re) {
+                throw new RuntimeException("예외 테스트...");
+            }
+
+            return result;
+        } finally {
+             long etime = System.nanoTime(); // 공통 기능
+            System.out.printf("걸린시간: %d%n", etime - stime);
+        }
     }
 }
