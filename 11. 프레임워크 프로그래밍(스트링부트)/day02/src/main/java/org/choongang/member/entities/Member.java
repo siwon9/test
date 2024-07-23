11d03aa4da1a9cc6c705c51a7b5fa7275cb78fe8
siwon9 <siwon10823@gmail.com>
2024-07-23 16:14:32 +0900

    7월23일 수업내용

--- 11. 프레임워크 프로그래밍(스트링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
+++ 11. 프레임워크 프로그래밍(스트링부트)/day02/src/main/java/org/choongang/member/entities/Member.java
@@ -2,6 +2,9 @@ package org.choongang.member.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.choongang.member.constants.Authority;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -14,11 +17,21 @@ import java.time.LocalDateTime;
         @Index(name="uq_email_passsword", columnList = "email, password", unique = true)
 })*/
 public class Member {
-    @Id @GeneratedValue
+    @Id @GeneratedValue//(strategy = GenerationType.AUTO)가 디폴트 값.
     private Long seq;
     private String email;
     private String password;
     private String userName;
+
+    //@Lob // 여러줄 써야할 때
+    @Transient // 내부적으로 쓸 목적이다. file.info를 보면 감을 잡을 수 있다.
+    private String introduction;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp // 둘다 기본값이 들어가진 않는다.
     private LocalDateTime modifiedAt;
 }
--- 11. 프레임워크 프로그래밍(스트링부트)/day02/src/test/java/org/choongang/jpa_study/Ex04.java
+++ 11. 프레임워크 프로그래밍(스트링부트)/day02/src/test/java/org/choongang/jpa_study/Ex04.java
@@ -5,43 +5,38 @@ import jakarta.persistence.PersistenceContext;
 import org.choongang.member.entities.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @SpringBootTest
 @Transactional
-
-public class Ex03 {
+@TestPropertySource(properties = "spring.profiles.active=test")
+public class Ex04 {
 
     @PersistenceContext
     private EntityManager em;
 
     @Test
-    void test1() {
+    void test1() throws Exception {
         Member member = new Member();
         member.setEmail("user01@test.org");
-        member.setPassword("12345678");
+        member.setPassword("12341234");
         member.setUserName("사용자01");
-        member.setCreatedAt(LocalDateTime.now());
 
         em.persist(member);
-
-        Member member2 = new Member();
-        member2.setEmail("user02@test.org");
-        member2.setPassword("12345678");
-        member2.setUserName("사용자02");
-        member2.setCreatedAt(LocalDateTime.now());
-
-        em.persist(member2);
-
         em.flush();
 
         em.clear();
 
-        Member _member1 = em.find(Member.class, member.getSeq());
-        System.out.println(_member1);
-        Member _member2 = em.find(Member.class, member2.getSeq());
-        System.out.println(_member2);
+        member = em.find(Member.class, member.getSeq());
+        System.out.println(member);
+
+        Thread.sleep(5000);
+        member.setUserName("(수정)사용자01");
+        em.flush();
+        em.clear();
+
+        member = em.find(Member.class, member.getSeq());
+        System.out.println(member);
     }
 }
