8255c99115ec0afa6c7e97ea7e8009a6d42b6bd2
siwon9 <siwon10823@gmail.com>
2024-07-23 12:03:53 +0900

    7월23일 수업내용

---
+++ 11. 프레임워크 프로그래밍(스트링부트)/day02/src/main/java/org/choongang/global/config/DBConfig.java
@@ -0,0 +1,7 @@
+package org.choongang.global.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DBConfig {
+}
--- 11. 프레임워크 프로그래밍(스트링부트)/day02/src/test/java/org/choongang/jpa_study/Ex01.java
+++ 11. 프레임워크 프로그래밍(스트링부트)/day02/src/test/java/org/choongang/jpa_study/Ex01.java
@@ -1,26 +1,31 @@
 package org.choongang.jpa_study;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.PersistenceContext;
 import org.choongang.member.entities.Member;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 @SpringBootTest
+@Transactional
 @TestPropertySource(properties = "spring.profiles.active=test") // 설정파일을 어떻게 가져올 것인지에 관하여
 public class Ex01 {
 
-    @Autowired
-    private EntityManagerFactory emf;
+    //@Autowired
+    //private EntityManagerFactory emf;
+
+    //@Autowired
+    @PersistenceContext // 영속성 주입을 위한 어노테이션
+    private EntityManager em; // 영속성 관리역할을 한다.
 
     @Test
     void test1() {
-        EntityManager em = emf.createEntityManager();// 상태변화에 따라서 쿼리가 자동실행되게 만든다.
+        //EntityManager em = emf.createEntityManager();// 상태변화에 따라서 쿼리가 자동실행되게 만든다.
 
         EntityTransaction tx = em.getTransaction();
         // transaction안에서 작업을 해줘야 작동이된다.
@@ -33,10 +38,24 @@ public class Ex01 {
         member.setUserName("user01");
         member.setCreatedAt(LocalDateTime.now());
 
-        em.persist(member);
+        em.persist(member); // 영속 상태 - 변화 감지 메모리에 있다, 변화감지중..
 
-        em.flush(); // 이게 뭘까
+        em.flush(); // INSERT 쿼리 // flush는 DB반영임. 쿼리가 추가될거같을 때마다 flush를 써야할듯
 
-        tx.commit(); // 예외가 발생하면 롤백하는식으로 try catch써도 될듯
+        em.detach(member); // 영속 상태 분리 - 변화 감지 X
+
+        member.setUserName("(수정)사용자01"); // 변경
+        member.setModifiedAt(LocalDateTime.now());
+
+        em.flush(); // UPDATE 쿼리 실행
+
+        em.merge(member);
+
+        em.flush();
+
+       // em.remove(member); // 제거 상태, 제거 X, 상태만 제거
+        //em.flush(); // DELETE 쿼리실행
+
+        //tx.commit(); // 예외가 발생하면 롤백하는식으로 try catch써도 될듯
     }
 }
