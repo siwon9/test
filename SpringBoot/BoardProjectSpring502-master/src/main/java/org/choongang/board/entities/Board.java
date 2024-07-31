2bb64c247950d50c6e0b7aa814ac1da136a772ed
siwon9 <siwon10823@gmail.com>
2024-07-31 15:04:54 +0900

    7/31 수업내용

---
+++ SpringBoot/BoardProjectSpring502-master/src/main/java/org/choongang/board/entities/Board.java
@@ -0,0 +1,24 @@
+package org.choongang.board.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.choongang.global.entities.BaseMemberEntity;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Board extends BaseMemberEntity { // 게시판 설정
+    @Id
+    @Column(length=30)
+    private String bId;
+
+    @Column(length=60, nullable = false)
+    private String bName;
+}
+
--- SpringBoot/BoardProjectSpring502-master/src/main/java/org/choongang/member/MemberUtil.java
+++ SpringBoot/BoardProjectSpring502-master/src/main/java/org/choongang/member/MemberUtil.java
@@ -1,9 +1,13 @@
 package org.choongang.member;
 
+import org.choongang.member.constants.Authority;
+import org.choongang.member.entities.Authorities;
 import org.choongang.member.entities.Member;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class MemberUtil {
@@ -13,8 +17,10 @@ public class MemberUtil {
     }
 
     public boolean isAdmin() {
-        if(isLogin()) {
-
+        if (isLogin()) {
+            Member member = getMember();
+            List<Authorities> authorities = member.getAuthorities();
+            return authorities.stream().anyMatch(s -> s.getAuthority() == Authority.ADMIN);
         }
 
         return false;
